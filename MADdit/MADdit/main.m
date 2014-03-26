f63501671f86b47614f01434b4157165aac70305
Comyar <comyarzaheri@utexas.edu>
2014-03-25 19:52:27 -0500

    Added project files

---
+++ MADdit/MADdit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MADdit
+//
+//  Created by Comyar Zaheri on 3/25/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MADAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MADAppDelegate class]));
+    }
+}
