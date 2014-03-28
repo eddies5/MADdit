736a54ebcde99c7aa49c7771e0e9e539f48509b7
Eddie <saenzeddie5@gmail.com>
2014-03-28 09:07:04 -0500

    added view controller

---
+++ MADdit/MADdit/MADMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADMainViewController.h
+//  MADdit
+//
+//  Created by Eddie Saenz on 3/28/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADMainViewController : UIViewController
+
+@end
